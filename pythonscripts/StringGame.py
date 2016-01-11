bdaf22de47aa9bc9856319d28ca497c1a31c7569
root <sr.kulkarni@icloud.com>
2016-01-11 12:06:10 -0800

    TC1.0 v2

--- pythonscripts/StringGame.py
+++ pythonscripts/StringGame.py
@@ -1,32 +1,49 @@
+import re
 
-def doStuff(initial,target):
-	flag = 1
-	while len(initial) < len(target) and flag ==1:
-		flag = 0
-		if initial+'A' in target:
-			initial = initial+'A'
-			flag = 1
-		changed = initial[::-1] 
- 		if changed+'B' in target: 
-			initial = changed + 'B'
-			flag =1
+class StringGame:
+
+	def __init__(self,string):
+		if not re.match(r'[AB]+',string):raise Exception("Input string is not in the correct format! Exiting..")
+		self.string = string	
+
+	def __repr__(self):
+		return self.string
+
+	def doStuff(self,targetObj):
+		flag = 1
+		initial = self.string
+		target = targetObj.string
+		while len(initial) < len(target) and flag ==1:
+			flag = 0
+			if initial+'A' in target:
+				initial = initial+'A'
+				flag = 1
+			changed = initial[::-1] 
+ 			if changed+'B' in target: 
+				initial = changed + 'B'
+				flag =1
 	
-	return (initial == target)
+		return (initial == target)
 
 
 
 
 
-initial = raw_input("Enter the initial string : ")
-target = raw_input("Enter the target string : ")
+if __name__ == '__main__':
 
-result = doStuff(initial,target)
+	str1 = raw_input("Enter the initial string : ")
+	str2 = raw_input("Enter the target string : ")
+        initial = StringGame(str1)
+        target = StringGame(str2)        
 
-print initial
-print target
+	result = initial.doStuff(target)
+        
+        print "Your strings are : "
+	print initial
+	print target
 
-if(result): print "Yo this is correct"
-else: print "This is not corect."
+	if(result): print "These strings satisfy our criteria"
+	else: print "These strings do not satisfy our criteria!"
 
 
 
