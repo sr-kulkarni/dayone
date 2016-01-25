4002aa76ae28c8db6de2b3841c8e0ebfcf3f8e41
Saurabh Kulkarni <Saurabh@Saurabhs-MacBook-Pro-3.local>
2016-01-24 16:02:10 -0800

    1/24 v2

--- pythonscripts/minLex.py
+++ pythonscripts/minLex.py
@@ -1,3 +1,5 @@
+import sys
+
 class MinStic:
 	
 	def __init__(self,string,string2):
@@ -9,15 +11,44 @@ class MinStic:
 		return str("'"+ self.firstString + "' , '" + self.nextString + "' ")
 
 	def doStuff(self):
-		print self
+		l1 = len(self.firstString)
+		l2 = len(self.nextString)
+		s1 = self.firstString
+		s2 = self.nextString
+		ans = []
+		i = 0
+		j = 0
+		while i < l1 or j < l2:
+			if(i == l1):
+				ans.append(s2[j:])
+				break
+			elif(j == l2):
+				ans.append(s1[i:])
+				break
+			elif(ord(s1[i]) < ord(s2[j])):
+				ans.append(s1[i])
+				i += 1
+			else:
+				ans.append(s2[j])
+				j += 1
+				
+
+		
+
+
+
+		print ''.join(ans)
+
+
 
 
 if __name__ == '__main__':
 	wordCount = input()
+	if wordCount < 1 or wordCount > 5: raise Exception()
+
 
 	for i in range(wordCount):
-		str1 = raw_input()
-		str2 = raw_input()
+		str1 = sys.stdin.readline().rstrip('\n')
+		str2 = sys.stdin.readline().rstrip('\n')
 		M = MinStic(str1,str2)
 		M.doStuff()
-	
